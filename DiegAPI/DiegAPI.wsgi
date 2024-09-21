ad1959f7432d6d0784038208c7ac0af1ceb471da
dygo454 <diegoaguilar454@yahoo.com>
2024-09-21 20:53:02 +0000

    did some work on the server side

---
+++ DiegAPI/DiegAPI.wsgi
@@ -0,0 +1,6 @@
+import sys
+import os
+
+sys.path.insert(0, '/var/www/html/portfolioWebsite/DiegAPI')
+
+from main import app as application
--- DiegAPI/main.py
+++ DiegAPI/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/ghAPI", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        subprocess.call(['sh', '../update.sh']) # in the futrure check if its from a push to main
+
+def create_app():
+   return app

267baf32ed4563e10add714dadbc0eaba17e56bf
Dygo454 <56135738+Dygo454@users.noreply.github.com>
2024-09-21 17:05:31 -0400

    taking away webhooks impl

--- DiegAPI/main.py
+++ DiegAPI/main.py
@@ -4,10 +4,5 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route("/ghAPI", methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        subprocess.call(['sh', '../update.sh']) # in the futrure check if its from a push to main
-
 def create_app():
    return app

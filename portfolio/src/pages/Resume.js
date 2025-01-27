2ebb9007550f0acb04c63b7a99d45739fcd9cd2a
Dygo454 <diegoaguilar454@yahoo.com>
2025-01-27 11:36:26 -0500

    Updated resume page

--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -14,18 +14,18 @@ function Resume() {
         <p><b>Bachelor of Science in Computer Science</b><span style={{float: "right"}}>August 2022 - May 2026</span></p>
         <p><i>University of Florida</i>, Gainesville, FL</p>
         <ul>
-            <li>GPA: 3.71 / 4.00</li>
-            <li>Relevant Coursework: Applications of Discrete Structures, Data Structures & Algorithms, Introduction to Computer Organization, Introduction to Software Engineering, Object-Oriented Programming, Study Abroad UF In Japan: Cross Cultural Design, Operating Systems.</li>
+            <li>GPA: 3.73 / 4.00</li>
+            <li>Relevant Coursework: Data Structures & Algorithms, Software Engineering, Object-Oriented Programming, Operating Systems, Algorithm Abstraction, Programming Language Concepts, Network Fundamentals, Information & Database Systems.</li>
         </ul>
         <br/>
         <h3>PROFESSIONAL EXPERIENCE</h3>
         <hr/>
-        <p><b>Teaching Assistant</b><span style={{float: "right"}}>January 2023 - Present</span></p>
-        <p><i>COP3503C | University of Florida</i>, Gainesville, FL</p>
+        <p><b>Backend Software Engineer</b><span style={{float: "right"}}>December 2024 - Present</span></p>
+        <p><i>ServeU</i>, Gainesville, FL</p>
         <ul>
-            <li>Managed a lab section which elaborated on core computer science topics, as well as demonstrated practical applications of concepts.</li>
-            <li>Assisted students on projects such as Minesweeper, while working on the backend to develop an auto-grader for the assignment.</li>
-            <li>Contributed on the backend of the course, helping with tasks ranging from course planning and assessments to creating labs.</li>
+            <li>Developed optimized backend systems for a customer-facing web app, focusing on efficient data processing and performance.</li>
+            <li>Collaborated with a team and led discussions to design and implement a functional web app to showcase to investors.</li>
+            <li>Led the migration from Flask to Firebase, reducing hosting costs by 90% and simplifying the frontend to backend communication.</li>
         </ul>
         <br/>
         <p><b>Teaching Assistant</b><span style={{float: "right"}}>September 2024 - Present</span></p>
@@ -35,20 +35,20 @@ function Resume() {
             <li>Analyzed course content to develop lecture slides, homework assignments, and assist in the creation and revisions of exams.</li>
         </ul>
         <br/>
-        <h3>Projects</h3>
-        <hr/>
-        <p><b>DGE (Diego Game Engine)</b><span style={{float: "right"}}>May 2021 - Present</span></p>
-        <p><i>Personal Project</i></p>
+        <p><b>Teaching Assistant</b><span style={{float: "right"}}>January 2023 - Present</span></p>
+        <p><i>COP3503C | University of Florida</i>, Gainesville, FL</p>
         <ul>
-            <li>Developed good habits and learned best practices for improving code modularity in a big project.</li>
-            <li>Learned many low level aspects of a game engine such as render pipelining by using only C++ and OpenCL.</li>
-            <li>Integrated OOP build patterns, such as fluid builders, to streamline creation of many large scale backend systems within the project.</li>
+            <li>Led weekly discussions to 30+ students, teaching core computer science topics and demonstrating their practical applications.</li>
+            <li>Developed an auto-grader in Gradescope for students' capstone project, reducing semester workload by 100+ hours.</li>
+            <li>Directed Peer Mentor teams to enhance assignment design and develop interactive lab materials, fostering improved student engagement and understanding of computer science concepts.</li>
         </ul>
         <br/>
+        <h3>Projects</h3>
+        <hr/>
         <p><b>Minesweeper Autograder</b><span style={{float: "right"}}>September 2024</span></p>
         <p><i>COP3503C - TA | University of Florida,  Gainesville, FL</i></p>
         <ul>
-            <li>Piloted an auto-grading library that when linked to student submissions, produces a score as well as a point breakdown.</li>
+            <li>Piloted an auto-grading library that when linked to student submissions, produces a score and point breakdown.</li>
             <li>Leveraged design patterns to keep track of code state and strategically organized the large code base of the library.</li>
         </ul>
         <br/>
@@ -57,6 +57,13 @@ function Resume() {
         <ul>
             <li>Collaborated with a team to create a game focused on Cross Cultural Design with the theme of disaster prevention.</li>
             <li>Integrated core mechanics utilizing design patterns, including decorators, commands, and mediators.</li>
+        </ul>
+        <br/>
+        <p><b>Spell Checker</b><span style={{float: "right"}}>November 2023</span></p>
+        <p><i>CEN3031 | University of Florida</i>,  Gainesville, FL</p>
+        <ul>
+            <li>Utilized Agile frameworks to develop a web application, managing workloads into sprints and CI/CD development.</li>
+            <li>Developed the backend of the application involving user authentication, database management, and OpenAI API calls.</li>
         </ul>
         <br/>
         <p><b>Spell Checker</b><span style={{float: "right"}}>November 2023</span></p>
@@ -71,6 +78,14 @@ function Resume() {
         <ul>
             <li>Strengthened familiarity with Unity and C# by designing and programming various games and participating in game jams.</li>
             <li>Planned and coded the implementations of various features in original games utilizing Unity Scripting API.</li>
+        </ul>
+        <br/>
+        <p><b>DGE (Diego Game Engine)</b><span style={{float: "right"}}>May 2021 - Present</span></p>
+        <p><i>Personal Project</i></p>
+        <ul>
+            <li>Developed good habits and learned best practices for improving code modularity in a big project.</li>
+            <li>Learned many low level aspects of a game engine such as render pipelining by using only C++ and OpenCL.</li>
+            <li>Integrated OOP build patterns, such as fluid builders, to streamline creation of many large scale backend systems within the project.</li>
         </ul>
         <br/>
         <p><b>Itch.io Games</b><span style={{float: "right"}}>May 2020 - Present</span></p>
